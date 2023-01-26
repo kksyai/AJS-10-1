0aa7c591df9e319244be278c0f597f1f99347b96
YanaRu <kksyai@gmail.com>
2023-01-26 19:01:31 +0300

    mmm

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -1,5 +1,5 @@
 import json from './parser.js';
-import read from '../reader.js';
+import read from './reader.js';
 
 export default class GameSavingLoader {
   load() {
@@ -17,3 +17,9 @@ export default class GameSavingLoader {
       });
   }
 }
+
+/* Для вас реализованы функции-заглушки, которые эмулируют чтение файла
+и преобразование прочитанного в JSON. Ваша задача — реализовать класс GameSavingLoader
+с методом load, который загружает данные с помощью функции read,
+парсит их с помощью функции json() и создаёт объект типа GameSaving.
+*/
